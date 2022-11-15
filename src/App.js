509a34e24b98273f96cd18215c3086ef48e78d45
Ben Walker <ben.walker@bjss.com>
2022-11-15 14:58:10 +0000

    now with posts!

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Posts from "./components/posts/Posts";
 
 function App() {
-  const { version } = window["appConfig"]?.version || "Demo";
+  const version = window["appConfig"]?.version || "Demo";
   return (
     <div className="container">
       <h1>Places App</h1>
--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,22 +1,38 @@
 import React, { useEffect, useState } from "react";
-
+import "./Posts.css";
 const Posts = () => {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState([true]);
 
   useEffect(() => {
     const getPosts = async () => {
+      setIsLoading(true);
+
       const response = await fetch(
         "https://jsonplaceholder.typicode.com/posts"
       );
       const data = await response.json();
 
       console.log("data", data);
+      setPosts(data);
+      setIsLoading(false);
     };
 
     getPosts();
   }, []);
 
-  return <div>Posts</div>;
+  return (
+    <div>
+      <h2>Posts</h2>
+
+      {isLoading && <span>Loading ...</span>}
+
+      <ul className="postList">
+        {!isLoading &&
+          posts.map((post, idx) => <li key={post.id}>Title: {post.title}</li>)}
+      </ul>
+    </div>
+  );
 };
 
 export default Posts;
