bce91b04acf71f76dfe3a9166a25afded48d7b7e
Ben Walker <ben.walker@bjss.com>
2022-11-15 11:30:22 +0000

    curly bracket mode activate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  const ( version ) = window['appConfig'] || 'demo';
+  const { version } = window["appConfig"] || "demo";
   return (
     <div className="App">
       Hello
-
       <h3>Version: {version}</h3>
     </div>
   );
